fd6f68c6cb7c121b1f2da54a7c06bd4ad81a73d6
corbinmoon <clangmoon@gmail.com>
2025-02-20 18:26:14 -0500

    move up a dir

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: /Users/corbinmoon/auxts/storage
+# Build directory: /Users/corbinmoon/auxts/storage/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[test_lru_cache]=] "auxts" "--test" "test_lru_cache")
+set_tests_properties([=[test_lru_cache]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/corbinmoon/auxts/storage/CMakeLists.txt;48;add_test;/Users/corbinmoon/auxts/storage/CMakeLists.txt;0;")
+add_test([=[test_multi_memtable]=] "auxts" "--test" "test_multi_memtable")
+set_tests_properties([=[test_multi_memtable]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/corbinmoon/auxts/storage/CMakeLists.txt;49;add_test;/Users/corbinmoon/auxts/storage/CMakeLists.txt;0;")
+add_test([=[test_extract]=] "auxts" "--test" "test_extract")
+set_tests_properties([=[test_extract]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/corbinmoon/auxts/storage/CMakeLists.txt;50;add_test;/Users/corbinmoon/auxts/storage/CMakeLists.txt;0;")
+add_test([=[test_hashtable]=] "auxts" "--test" "test_hashtable")
+set_tests_properties([=[test_hashtable]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/corbinmoon/auxts/storage/CMakeLists.txt;51;add_test;/Users/corbinmoon/auxts/storage/CMakeLists.txt;0;")
+add_test([=[test_murmurhash3]=] "auxts" "--test" "test_murmurhash3")
+set_tests_properties([=[test_murmurhash3]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/corbinmoon/auxts/storage/CMakeLists.txt;52;add_test;/Users/corbinmoon/auxts/storage/CMakeLists.txt;0;")
+add_test([=[test_rfc3339]=] "auxts" "--test" "test_rfc3339")
+set_tests_properties([=[test_rfc3339]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/corbinmoon/auxts/storage/CMakeLists.txt;53;add_test;/Users/corbinmoon/auxts/storage/CMakeLists.txt;0;")
