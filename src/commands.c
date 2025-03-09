d6e8262ddc11660ec7ef217c8b5bbf32511bf459
corbinmoon <clangmoon@gmail.com>
2025-03-08 19:50:42 -0500

    refactoring

--- src/commands.c
+++ src/commands.c
@@ -14,7 +14,12 @@ auxts_create_command(extract) {
     int64_t from, to;
 
     auxts_validate_arg_type(&pk, msg, 0, MSGPACK_OBJECT_STR, "Invalid type at arg 1. Expected: string")
-    auxts_validate(&pk, auxts_deserialize_range(&from, &to, &msg.data), "Invalid RFC-3339 timestamp. Expected format: yyyy-MM-ddTHH:mm:ss.SSSZ")
+    auxts_validate_arg_type(&pk, msg, 1, MSGPACK_OBJECT_STR, "Invalid type at arg 2. Expected: string")
+    auxts_validate_arg_type(&pk, msg, 2, MSGPACK_OBJECT_STR, "Invalid type at arg 3. Expected: string")
+
+    auxts_validate(&pk, auxts_deserialize_range(&from, &to, &msg.data),
+                   "Invalid RFC-3339 timestamp. Expected format: yyyy-MM-ddTHH:mm:ss.SSSZ")
+
     auxts_deserialize_stream_id(&stream_id, &msg.data, 0);
 
     auxts_pcm_buffer pbuf;
