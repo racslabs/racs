f1277e1a6e9b200f1a6b1097718fe8f245241e89
corbinmoon <clangmoon@gmail.com>
2025-03-28 15:15:20 -0400

    update atsp

--- src/atsp.c
+++ src/atsp.c
@@ -1,1 +1,18 @@
+#include "atsp.h"
 
+int auxts_atsp_header_parse(int socket_fd, auxts_atsp_header* header) {
+    uint8_t buf[26];
+
+    if (recv(socket_fd, buf, 26, MSG_WAITALL) != 26)
+        return false;
+
+    memcpy(&header->chunk_id, buf, 4);
+    auxts_read_uint64(&header->hash, buf, 4);
+    auxts_read_uint32(&header->checksum, buf, 12);
+    auxts_read_uint16(&header->channels, buf, 16);
+    auxts_read_uint32(&header->sample_rate, buf, 18);
+    auxts_read_uint16(&header->bit_depth, buf, 22);
+    auxts_read_uint16(&header->block_size, buf, 24);
+
+    return true;
+}
--- src/atsp.h
+++ src/atsp.h
@@ -2,7 +2,9 @@
 #ifndef AUXTS_ATSP_H
 #define AUXTS_ATSP_H
 
-#include <stdint.h>
+#include <sys/socket.h>
+#include <stdbool.h>
+#include "bytes.h"
 
 typedef struct {
     char chunk_id[4];
@@ -12,7 +14,13 @@ typedef struct {
     uint32_t sample_rate;
     uint16_t bit_depth;
     uint16_t block_size;
+} auxts_atsp_header;
+
+typedef struct {
+    auxts_atsp_header header;
     uint8_t* pcm_block;
 } auxts_atsp_frame;
+
+int auxts_atsp_header_parse(int socket_fd, auxts_atsp_header* header);
 
 #endif //AUXTS_ATSP_H
