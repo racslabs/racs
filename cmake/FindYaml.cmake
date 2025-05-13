1ba97f333c229969c4edacb405ec2ea5cd3b4258
corbinmoon <clangmoon@gmail.com>
2025-05-12 22:38:57 -0400

    update cmake module

--- cmake/FindYaml.cmake
+++ cmake/FindYaml.cmake
@@ -1,7 +1,9 @@
 find_path (YAML_INCLUDE_DIR cyaml.h
         PATH_SUFFIXES
-        /yaml
-        HINTS /opt/local/include
+        /cyaml
+        HINTS
+        /opt/local/include
+        /opt/homebrew/include
 )
 
 # Look for the library
@@ -14,4 +16,5 @@ find_library (YAML_LIBRARY
 set (YAML_LIBRARIES ${YAML_LIBRARY})
 set (YAML_INCLUDE_DIRS ${YAML_INCLUDE_DIR})
 
+message(STATUS "YAML inc:" ${YAML_INCLUDE_DIRS})
 message(STATUS "libYAML: ${YAML_LIBRARIES}")
