587cf79575b97ac4e12f0bb6fa722bb993f9caf4
corbinmoon <clangmoon@gmail.com>
2025-10-06 20:02:09 -0400

    scm binding for FORMAT

--- src/scm_bindings.c
+++ src/scm_bindings.c
@@ -36,12 +36,13 @@ SCM racs_scm_extract(SCM stream_id, SCM from, SCM to) {
     return scm_take_s32vector(data, size);
 }
 
-SCM racs_scm_streamcreate(SCM stream_id, SCM sample_rate, SCM channels) {
+SCM racs_scm_streamcreate(SCM stream_id, SCM sample_rate, SCM channels, SCM bit_depth) {
     char *cmd = NULL;
-    asprintf(&cmd, "CREATE '%s' %d %d",
+    asprintf(&cmd, "CREATE '%s' %d %d %d",
              scm_to_locale_string(stream_id),
              scm_to_uint32(sample_rate),
-             scm_to_uint32(channels));
+             scm_to_uint32(channels),
+             scm_to_uint32(bit_depth));
 
     racs_db *db = racs_db_instance();
     racs_result res = racs_db_exec(db, cmd);
@@ -66,7 +67,7 @@ SCM racs_scm_streamcreate(SCM stream_id, SCM sample_rate, SCM channels) {
 
 SCM racs_scm_streaminfo(SCM stream_id, SCM attr) {
     char *cmd = NULL;
-    asprintf(&cmd, "RACS_LOG_INFO '%s' '%s'",
+    asprintf(&cmd, "INFO '%s' '%s'",
              scm_to_locale_string(stream_id),
              scm_to_locale_string(attr));
 
@@ -93,12 +94,38 @@ SCM racs_scm_streaminfo(SCM stream_id, SCM attr) {
         return SCM_EOL;
     }
 
-    racs_uint64 value = racs_unpack_uint64(&msg.data, 1);
-    return scm_from_uint64(value);
+    racs_int64 value = racs_unpack_int64(&msg.data, 1);
+    return scm_from_int64(value);
+}
+
+SCM racs_scm_format(SCM data, SCM mime_type, SCM sample_rate, SCM channels, SCM bit_depth) {
+    char *_mime_type = scm_to_locale_string(mime_type);
+    racs_uint32 _sample_rate = scm_to_uint32(sample_rate);
+    racs_uint16 _channels = scm_to_uint16(channels);
+    racs_uint16 _bit_depth = scm_to_uint16(bit_depth);
+
+    scm_t_array_handle handle;
+    scm_array_get_handle(data, &handle);
+
+    const racs_int32 *in = scm_array_handle_s32_elements(&handle);
+    size_t size = scm_c_array_length(data);
+
+    void *out = malloc(size * (_bit_depth / 8) + 44);
+
+    racs_format fmt;
+    fmt.channels = _channels;
+    fmt.sample_rate = _sample_rate;
+    fmt.bit_depth = _bit_depth;
+
+    size_t n = racs_format_pcm(&fmt, in, out, size / _channels, size * (_bit_depth / 8) + 44, _mime_type);
+
+    scm_array_handle_release(&handle);
+    return scm_take_u8vector((uint8_t *)out, n);
 }
 
 void racs_scm_init_bindings() {
     scm_c_define_gsubr("extract", 3, 0, 0, racs_scm_extract);
     scm_c_define_gsubr("create", 4, 0, 0, racs_scm_streamcreate);
     scm_c_define_gsubr("info", 2, 0, 0, racs_scm_streaminfo);
+    scm_c_define_gsubr("format", 5, 0, 0, racs_scm_format);
 }
--- src/scm_bindings.h
+++ src/scm_bindings.h
@@ -7,9 +7,11 @@
 
 SCM racs_scm_extract(SCM stream_id, SCM from, SCM to);
 
-SCM racs_scm_streamcreate(SCM stream_id, SCM sample_rate, SCM channels);
+SCM racs_scm_streamcreate(SCM stream_id, SCM sample_rate, SCM channels, SCM bit_depth);
 
 SCM racs_scm_streaminfo(SCM stream_id, SCM attr);
+
+SCM racs_scm_format(SCM data, SCM mime_type, SCM sample_rate, SCM channels, SCM bit_depth);
 
 void racs_scm_init_bindings();
 
