41f6d44ae0c9c7ce3f725acc71b3f9659e0fe870
corbinmoon <clangmoon@gmail.com>
2025-12-08 14:58:34 -0500

    use libevent in server

--- src/server.c
+++ src/server.c
@@ -58,201 +58,70 @@ void racs_args(int argc, char *argv[]) {
     exit(-1);
 }
 
-void racs_init_socket(racs_conn *conn) {
-    // Create an AF_INET6 stream socket to receive incoming
-    // connections
-    conn->listen_sd = socket(AF_INET6, SOCK_STREAM, 0);
-    if (conn->listen_sd < 0) {
-        racs_log_fatal("socket() failed");
-        exit(-1);
+void racs_read_callback(struct bufferevent *bev, void *ctx) {
+    racs_db *db = ctx;
+
+    struct evbuffer *in  = bufferevent_get_input(bev);
+    struct evbuffer *out = bufferevent_get_output(bev);
+
+    while (1) {
+        size_t size = evbuffer_get_length(in);
+        if (size < 8) return;
+
+        racs_uint8 *header = evbuffer_pullup(in, 8);
+
+        racs_uint64 payload_len;
+        memcpy(&payload_len, header, 8);
+
+        if (size < 8 + payload_len) return;
+
+        evbuffer_drain(in, 8);
+
+        racs_uint8 *buf = malloc(payload_len);
+        evbuffer_remove(in, buf, payload_len);
+
+        racs_result res;
+        if (racs_is_frame((const char *) buf))
+            res = racs_db_stream(db, buf);
+        else
+            res = racs_db_exec(db, (const char *) buf);
+
+        free(buf);
+
+        racs_uint8 header_out[8];
+        memcpy(header_out, &res.size, 8);
+
+        evbuffer_add(out, header_out, 8);
+        evbuffer_add(out, res.data, res.size);
+
+        free(res.data);
     }
 }
 
-void racs_set_socketopts(racs_conn *conn) {
-    int on = 1, off = 0, rc;
-    int bufsize = 1024*1024; // 1 MB
+void racs_event_callback(struct bufferevent *bev, short events, void *ctx) {
+    if (events & BEV_EVENT_ERROR)
+        racs_log_error("Error on bufferevent");
 
-    // Allow socket descriptor to be reusable
-    rc = setsockopt(conn->listen_sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-    if (rc < 0) {
-        racs_log_fatal("setsockopt() failed");
-        close(conn->listen_sd);
-        exit(-1);
-    }
-
-    // Disable IPV6 only.
-    rc = setsockopt(conn->listen_sd, IPPROTO_IPV6, IPV6_V6ONLY, &off, sizeof(off));
-    if (rc < 0) {
-        racs_log_fatal("setsockopt(IPV6_V6ONLY) failed");
-        close(conn->listen_sd);
-        exit(-1);
-    }
-
-    rc = setsockopt(conn->listen_sd, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(on));
-    if (rc < 0) {
-        perror("setsockopt TCP_NODELAY failed");
-        close(conn->listen_sd);
-        exit(-1);
-    }
-
-    rc = setsockopt(conn->listen_sd, SOL_SOCKET, SO_SNDBUF, &bufsize, sizeof(bufsize));
-    if (rc < 0) {
-        perror("setsockopt SO_SNDBUF failed");
-        close(conn->listen_sd);
-        exit(-1);
-    }
-
-    rc = setsockopt(conn->listen_sd, SOL_SOCKET, SO_RCVBUF, &bufsize, sizeof(bufsize));
-    if (rc < 0) {
-        perror("setsockopt SO_RCVBUF failed");
-        close(conn->listen_sd);
-        exit(-1);
-    }
+    if (events & (BEV_EVENT_EOF | BEV_EVENT_ERROR))
+        bufferevent_free(bev);
 }
 
-void racs_set_nonblocking(racs_conn *conn) {
-    int rc, on = 1;
+void racs_accept_callback(struct evconnlistener *listener, evutil_socket_t fd, struct sockaddr *addr, int socklen, void *data) {
+    racs_connection_context *ctx = data;
 
-    // Set socket to be nonblocking. All the sockets for
-    // the incoming connections will also be nonblocking since
-    // they will inherit that state from the listening socket.
-    rc = ioctl(conn->listen_sd, FIONBIO, &on);
-    if (rc < 0) {
-        racs_log_fatal("ioctl() failed");
-        close(conn->listen_sd);
-        exit(-1);
-    }
-}
+    struct event_base *base = ctx->base;
+    evutil_make_socket_nonblocking(fd);
 
-void racs_socket_bind(racs_conn *conn, int port) {
-    memset(&conn->addr, 0, sizeof(conn->addr));
-    conn->addr.sin6_family = AF_INET6;
-    memcpy(&conn->addr.sin6_addr, &in6addr_any, sizeof(in6addr_any));
-    conn->addr.sin6_port = htons(port);
+    struct bufferevent *bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
+    bufferevent_setcb(bev, racs_read_callback, NULL, racs_event_callback, ctx->db);
+    bufferevent_enable(bev, EV_READ | EV_WRITE);
 
-    int rc = bind(conn->listen_sd, (struct sockaddr *) &conn->addr, sizeof(conn->addr));
-    if (rc < 0) {
-        racs_log_fatal("bind() failed");
-        close(conn->listen_sd);
-        exit(-1);
-    }
-}
-
-void racs_socket_listen(racs_conn *conn) {
-    int rc = listen(conn->listen_sd, 32);
-    if (rc < 0) {
-        racs_log_fatal("listen() failed");
-        close(conn->listen_sd);
-        exit(-1);
-    }
-}
-
-void racs_conn_stream_init(racs_conn_stream *stream) {
-    racs_memstream_init(&stream->in_stream);
-    racs_memstream_init(&stream->out_stream);
-}
-
-void racs_conn_stream_reset(racs_conn_stream *stream) {
-    free(stream->in_stream.data);
-    free(stream->out_stream.data);
-
-    racs_conn_stream_init(stream);
-}
-
-int racs_recv_length_prefix(int fd, size_t *len) {
-    int rc;
-
-    rc = recv(fd, len, sizeof(size_t), 0);
-    if (rc < 0) {
-        if (errno != EWOULDBLOCK) {
-            racs_log_fatal("recv() failed: %s", strerror(errno));
-            return -1;
-        }
-        return 0;
-    }
-
-    if (rc == 0) {
-        racs_log_info("  Connection closed");
-        return -1;
-    }
-
-    return 8;
-}
-
-int racs_recv(int fd, int len, racs_conn_stream *stream) {
-    int rc;
-    char buf[RACS_CHUNK_SIZE];
-
-    while (stream->in_stream.current_pos < len) {
-        rc = recv(fd, buf, sizeof(buf), 0);
-
-        if (rc < 0) {
-            if (errno != EWOULDBLOCK) {
-                racs_log_fatal("recv() failed: %s", strerror(errno));
-                return -1;
-            }
-            continue;
-        }
-
-        if (rc == 0) {
-            racs_log_info("  Connection closed");
-            return -1;
-        }
-
-        racs_memstream_write(&stream->in_stream, buf, rc);
-    }
-
-    return stream->in_stream.current_pos;
-}
-
-int racs_send(int fd, racs_conn_stream *stream) {
-    size_t bytes = 0;
-    size_t n = stream->out_stream.current_pos;
-    ssize_t rc;
-
-    signal(SIGPIPE, SIG_IGN);
-
-    while (bytes < n) {
-        size_t rem = n - bytes;
-        size_t to_send = (rem < RACS_CHUNK_SIZE) ? rem : RACS_CHUNK_SIZE;
-
-        rc = send(fd, stream->out_stream.data + bytes, to_send, MSG_NOSIGNAL);
-        if (rc < 0) {
-            if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                continue;
-            } else if (errno == EPIPE) {
-                racs_log_error("peer closed connection: %s", strerror(errno));
-                return -1;
-            } else {
-                racs_log_error("send() failed: %s", strerror(errno));
-                return -1;
-            }
-        }
-
-        if (rc == 0) {
-            racs_log_error("send() returned 0 (peer closed?)");
-            return -1;
-        }
-
-        bytes += (size_t)rc;
-    }
-
-    return 0;
+    racs_log_info("Client connected");
 }
 
 int main(int argc, char *argv[]) {
-    int rc;
-    int new_sd = -1;
-    int end = false, compress = false;
-    int close_conn;
-    int timeout;
-    struct pollfd fds[200];
-    int nfds = 1, current_size = 0, i, j;
 
     racs_args(argc, argv);
-
-    racs_conn conn;
-    racs_conn_stream streams[200];
 
     scm_init_guile();
     racs_scm_init_module();
@@ -265,173 +134,47 @@ int main(int argc, char *argv[]) {
     racs_version(ver);
     racs_log_info(ver);
 
-    racs_init_socket(&conn);
-    racs_set_socketopts(&conn);
-    racs_set_nonblocking(&conn);
+    signal(SIGPIPE, SIG_IGN);
 
-    racs_socket_bind(&conn, db->ctx.config->port);
-    racs_socket_listen(&conn);
+    racs_connection_context ctx;
+    ctx.db = db;
+    ctx.base = event_base_new();
 
-    // Initialize the pollfd structure
-    memset(fds, 0, sizeof(fds));
-
-    // Set up the initial listening socket
-    fds[0].fd = conn.listen_sd;
-    fds[0].events = POLLIN;
-
-    timeout = -1;
-
-    racs_log_info("Listening on port %d ...", db->ctx.config->port);
-    racs_log_info("Log file: %s/racs.log", racs_log_dir);
-
-    // Loop waiting for incoming connects or for incoming data
-    // on any of the connected sockets.
-    do {
-        // Call poll() and wait for it to complete.
-        rc = poll(fds, nfds, timeout);
-
-        // Check to see if the poll call failed.
-        if (rc < 0) {
-            racs_log_error("  poll() failed");
-            break;
-        }
-
-        // Check to see if time out expired.
-        if (rc == 0) {
-            racs_log_error("  poll() timed out.  End program.");
-            break;
-        }
-
-        // One or more descriptors are readable.  Need to
-        // determine which ones they are.
-        current_size = nfds;
-        for (i = 0; i < current_size; i++) {
-
-            // Loop through to find the descriptors that returned
-            // POLLIN and determine whether it's the listening
-            // or the active connection.
-            if (fds[i].revents == 0)
-                continue;
-
-            if (fds[i].revents & POLLHUP) {
-                close(fds[i].fd);
-                fds[i].fd = -1;
-                continue;
-            }
-
-            if (fds[i].revents & POLLERR) {
-                close(fds[i].fd);
-                fds[i].fd = -1;
-                continue;
-            }
-
-            // If revents is not POLLIN, it's an unexpected result,
-            // log and end the server.
-            if (fds[i].revents != POLLIN) {
-                racs_log_fatal("  Error! revents = %d", fds[i].revents);
-                end = true;
-                break;
-            }
-
-            if (fds[i].fd == conn.listen_sd) {
-
-                // Accept all incoming connections that are
-                // queued up on the listening socket before we
-                // loop back and call poll again.
-                do {
-                    // Accept each incoming connection. If
-                    // accept fails with EWOULDBLOCK, then we
-                    // have accepted all of them. Any other
-                    // failure on accept will cause us to end the
-                    // server.
-                    new_sd = accept(conn.listen_sd, NULL, NULL);
-                    if (new_sd < 0) {
-                        if (errno != EWOULDBLOCK) {
-                            racs_log_fatal("  accept() failed");
-                            end = true;
-                        }
-                        break;
-                    }
-
-                    // Add the new incoming connection to the
-                    // pollfd structure
-                    fds[nfds].fd = new_sd;
-                    fds[nfds].events = POLLIN;
-                    racs_conn_stream_init(&streams[nfds]);
-                    nfds++;
-                } while (1);
-            } else {
-                // Receive all incoming data on this socket
-                // before we loop back and call poll again.
-                close_conn = false;
-                size_t length = 0;
-
-                rc = racs_recv_length_prefix(fds[i].fd, &length);
-                if (rc < 0) close_conn = true;
-
-                rc = racs_recv(fds[i].fd, (int) length, &streams[i]);
-                if (rc < 0) close_conn = true;
-
-                // Echo the data back to the client
-                if (streams[i].in_stream.current_pos > 0) {
-                    racs_result res;
-
-                    if (RACS_IS_FRAME((const char *) streams[i].in_stream.data)) {
-                        res = racs_db_stream(db, streams[i].in_stream.data);
-                    } else {
-                        res = racs_db_exec(db, (const char *) streams[i].in_stream.data);
-                    }
-
-                    racs_memstream_write(&streams[i].out_stream, &res.size, sizeof(size_t));
-                    racs_memstream_write(&streams[i].out_stream, res.data, res.size);
-                    free(res.data);
-
-                    rc = racs_send(fds[i].fd, &streams[i]);
-
-                    if (rc < 0) {
-                        racs_conn_stream_reset(&streams[i]);
-                        close_conn = true;
-                        break;
-                    }
-
-                    racs_conn_stream_reset(&streams[i]);
-                }
-
-                // If the close_conn flag was turned on, we need
-                // to clean up this active connection. This
-                // clean up process includes removing the
-                // descriptor.
-                if (close_conn) {
-                    close(fds[i].fd);
-                    fds[i].fd = -1;
-                    compress = true;
-                }
-            }
-        }
-
-        // If the compress flag was turned on, we need
-        // to squeeze together the array and decrement the number
-        // of file descriptors. We do not need to move back the
-        // events and revents fields because the events will always
-        // be POLLIN in this case, and revents is output.
-        if (compress) {
-            compress = false;
-            for (i = 0; i < nfds; i++) {
-                if (fds[i].fd == -1) {
-                    for (j = i; j < nfds - 1; j++) {
-                        fds[j].fd = fds[j + 1].fd;
-                    }
-                    i--;
-                    nfds--;
-                }
-            }
-        }
-
-    } while (end == false);
-
-    // Clean up all the sockets that are open
-    for (i = 0; i < nfds; i++) {
-        if (fds[i].fd >= 0)
-            close(fds[i].fd);
+    if (!ctx.base) {
+        racs_log_fatal("Could not initialize libevent.");
+        return 1;
     }
+
+    /* Listening address */
+    struct sockaddr_in sin;
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = htonl(0);   /* 0.0.0.0 */
+    sin.sin_port = htons(db->ctx.config->port);
+
+    /* Create listener */
+    struct evconnlistener *listener = evconnlistener_new_bind(
+        ctx.base,
+        racs_accept_callback,               /* on new connection */
+        &ctx,                    /* context for callback */
+        LEV_OPT_REUSEABLE | LEV_OPT_CLOSE_ON_FREE,
+        -1,                      /* backlog */
+        (struct sockaddr*)&sin,
+        sizeof(sin)
+    );
+
+    if (!listener) {
+        racs_log_fatal("Could not create listener");
+        return 1;
+    }
+
+    racs_log_info("Server listening on port %d...", db->ctx.config->port);
+
+    /* Start event loop (runs forever) */
+    event_base_dispatch(ctx.base);
+
+    evconnlistener_free(listener);
+    event_base_free(ctx.base);
+    return 0;
+
 }
--- src/server.h
+++ src/server.h
@@ -10,14 +10,16 @@
 #ifndef RACS_SERVER_H
 #define RACS_SERVER_H
 
+#include <event2/event.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/listener.h>
+#include <event2/util.h>
 
-#include <sys/types.h>      // for socket types
-#include <sys/socket.h>     // for socket, connect, send, recv
-#include <sys/ioctl.h>      // for ioctl, FIONBIO
-#include <netinet/in.h>     // for sockaddr_in, htons, etc.
-#include <netinet/tcp.h>    // for TCP_FASTOPEN
-#include <arpa/inet.h>      // for inet_pton, inet_ntop
-#include <poll.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 
 #include "db.h"
 #include "scm_bindings.h"
@@ -25,38 +27,19 @@
 #include "version.h"
 
 typedef struct {
-    int fd;
-    racs_memstream in_stream;
-    racs_memstream out_stream;
-} racs_conn_stream;
-
-typedef struct {
-    int listen_sd, new_sd;
-    struct sockaddr_in6 addr;
-} racs_conn;
+    struct event_base *base;
+    racs_db *db;
+} racs_connection_context;
 
 void racs_help();
 
 void racs_args(int argc, char *argv[]);
 
-void racs_init_socket(racs_conn *conn);
+void racs_read_callback(struct bufferevent *bev, void *ctx);
 
-void racs_set_socketopts(racs_conn *conn);
+void racs_event_callback(struct bufferevent *bev, short events, void *ctx);
 
-void racs_set_nonblocking(racs_conn *conn);
+void racs_accept_callback(struct evconnlistener *listener, evutil_socket_t fd, struct sockaddr *addr, int socklen, void *data);
 
-void racs_socket_bind(racs_conn *conn, int port);
-
-void racs_socket_listen(racs_conn *conn);
-
-void racs_conn_stream_init(racs_conn_stream *stream);
-
-void racs_conn_stream_reset(racs_conn_stream *stream);
-
-int racs_recv_length_prefix(int fd, size_t *len);
-
-int racs_recv(int fd, int len, racs_conn_stream *stream);
-
-int racs_send(int fd, racs_conn_stream *stream);
 
 #endif //RACS_SERVER_H
